d56c9dc3ed3b98e9ff76fb1fd56ba652d4ed4801
asbarros <anderson.barros0298@gmail.com>
2020-06-04 23:42:06 -0300

    index principal

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-require('./lib/index')
+const { program } = require('commander')
+const shell = require('shelljs');
+program.version('1.0.0')
+
+program
+    .command('simple-structure create')
+    .description('clone a repository into a newly created directory')
+    .action((source, destination) => {
+        console.log('creating...');
+        shell.cp('-R', 'lib/structure', 'app');
+        console.log('instalando dependencias...')
+        shell.exec('npm install express')
+        shell.exec('npm install consign')
+        shell.exec('npm install cors')
+        shell.exec('npm install knex')
+        shell.exec('npm install body-parse')
+        shell.exec('npm install passport')
+        shell.exec('npm install passport-jwt')
+        shell.exec('npm install jwt-simple')
+        shell.exec('npm install bcrypt-nodejs')
+    });
+
+program.parse(process.argv);
